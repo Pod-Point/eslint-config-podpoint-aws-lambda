69362d3473755f286aa8189a9aaac139d57782f1
Mike Francis <mikeffrancis@gmail.com>
2017-03-02 14:57:15 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "podpoint-base",
+    "rules": {
+        "strict": "off",
+        "eqeqeq": "off",
+        "no-unused-vars": "off",
+        "class-methods-use-this": "off",
+        "no-console": "off",
+        "comma-dangle": ["error", "ignore"]
+    }
+};
